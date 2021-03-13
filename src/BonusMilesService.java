87e812e835ea494e7815e2febc4159b6fc3ceac4
Irina Salovskaya <salovskaya@yandex.ru>
2021-03-13 15:59:30 +0300

    Added Main.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,14 +1,9 @@
 public class BonusMilesService {
     public int calculate (int price ){
-        public class Main {
-            public static void main(String[] args) {
-                int price = 10_000;
-                int bonus = 20;
 
-                int miles = price/bonus;
-                System.out.println(miles);
+        int bonus = 20;
+        int miles = price/bonus;
+        return miles;
             }
         }
-        return int miles;
-    }
-}
+
